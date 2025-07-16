4de41b5cfe68cb240dc3305ec316a93c23c9c55d
Kyne <kyne.li@hotmail.com>
2025-07-16 19:14:51 +0800

    初始化代码提交

---
+++ src/main/java/org/jeecg/boot/camunda7/JeecgApplication.java
@@ -0,0 +1,13 @@
+package org.jeecg.boot.camunda7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeecgApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(JeecgApplication.class, args);
+  }
+
+}
